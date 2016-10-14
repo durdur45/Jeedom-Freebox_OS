b69e17a9de07a28b9a958b0f0882eb3e11c8d850
Halbout Michael <michaelhalbout@gmail.com>
2016-10-14 14:08:56 +0200

    Update Freebox_OS.js

--- desktop/js/Freebox_OS.js
+++ desktop/js/Freebox_OS.js
@@ -58,9 +58,11 @@ function addCmdToTable(_cmd) {
 			.append($('<input type="hidden" class="cmdAttr" data-l1key="subType" />'))	
 		.append($('<div style="width : 40%;display : inline-block;">')
 			.append($('<span>')
+				.append('{{Historiser}}')
 				.append($('<input type="checkbox" class="cmdAttr" data-size="mini" data-label-text="{{Historiser}}" data-l1key="isHistorized" checked/>')))
 			.append($('</br>'))
 			.append($('<span>')
+				.append('{{Afficher}}')
 				.append($('<input type="checkbox" class="cmdAttr" data-size="mini" data-label-text="{{Afficher}}" data-l1key="isVisible" checked/>'))))
 			.append($('<div style="width : 40%;display : inline-block;">')	
 				.append($('<input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="{{Unité}}" title="Unité"/>'))));  
