62e2446ba2aadff7438f3e28ba93b5479b2a575c
Halbout Michael <michaelhalbout@gmail.com>
2016-11-07 14:38:03 +0100

    Update Freebox_OS.php

--- desktop/php/Freebox_OS.php
+++ desktop/php/Freebox_OS.php
@@ -6,49 +6,64 @@ sendVarToJS('eqType', 'Freebox_OS');
 $eqLogics = eqLogic::byType('Freebox_OS');
 ?>
 <div class="row row-overflow">
-    <div class="col-md-2">
+    <div class="col-lg-2 col-md-3 col-sm-4">
         <div class="bs-sidebar">
             <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un player}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
-                foreach (eqLogic::byType('Freebox_OS') as $eqLogic) {
-					echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
                 }
                 ?>
             </ul>
         </div>
     </div>
-	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-        <legend>{{Mes informations Freebox}}
-        </legend>
-        <?php
-        if (count($eqLogics) == 0) {
-            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Aucun equipement n'a été créer.}} <br/>{{Veuillez D
-			désactuiver puis re-activer le plugin pour la création automatique des équipement}}</span></center>";
-        } else {
-            ?>
-            <div class="eqLogicThumbnailContainer">
-                <?php
-                foreach ($eqLogics as $eqLogic) {
-                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-                    echo "<center>";
-                    echo '<img src="plugins/Freebox_OS/doc/images/Freebox_OS_icon.png" height="105" width="95" />';
-                    echo "</center>";
-                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-                    echo '</div>';
-                }
-                ?>
-            </div>
-        <?php } ?>
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">	
+		<legend>{{Gestion}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; " >
+				<center>
+					<i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+				</center>
+				<span style="font-size : 1.1em;position:relative; word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">
+					<center>Ajouter</center>
+				</span>
+			</div>
+			<div class="cursor eqLogicAction" data-action="gotoPluginConf" style="height: 120px; margin-bottom: 10px; padding: 5px; border-radius: 2px; width: 160px; margin-left: 10px; position: absolute; left: 170px; top: 0px; background-color: rgb(255, 255, 255);">
+				<center>
+			      		<i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+			    	</center>
+			    	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>Configuration</center></span>
+			</div>
+		</div>
+        <legend>{{Mes camera surveiller}}</legend>
+		<div class="eqLogicThumbnailContainer">
+			<?php
+			if (count($eqLogics) == 0) {
+				echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Aucun equipement n'a été créer, veuillez D
+			désactiver puis re-activer le plugin pour la création automatique des équipement}}</span></center>";
+			} else {
+			?>
+				<?php
+				foreach ($eqLogics as $eqLogic) {
+					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+					echo "<center>";
+					echo '<img src="plugins/Freebox_OS/doc/images/Freebox_OS_icon.png" height="105" width="95" />';
+					echo "</center>";
+					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+					echo '</div>';
+				}
+				?>
+			<?php } ?>
+		</div>
     </div>
-    <div class="col-md-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
         <form class="form-horizontal">
-            <fieldset>
+            <fieldset>     
 				<legend>
 						<i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> Général  
 						<a class="btn btn-default btn-xs pull-right eqLogicAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i> Configuration avancée</a>
-						<a class="btn btn-default btn-xs pull-right eqLogicAction expertModeVisible" data-action="copy"><i class="fa fa-copy"></i>Dupliquer</a>
 						<a class="btn btn-success btn-xs eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
 						<a class="btn btn-danger btn-xs eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
 				</legend>
@@ -72,8 +87,7 @@ $eqLogics = eqLogic::byType('Freebox_OS');
                             ?>
                         </select>
                     </div>
-                </div>
-				
+                </div>			
 				<div class="form-group">
                     <label class="col-md-2 control-label">{{Catégorie}}</label>
                     <div class="col-md-8">
@@ -84,7 +98,6 @@ $eqLogics = eqLogic::byType('Freebox_OS');
                             echo '</label>';
                         }
                         ?>
-
                     </div>
                 </div>  
 				<div class="form-group">
@@ -129,17 +142,15 @@ $eqLogics = eqLogic::byType('Freebox_OS');
             <tbody>
             </tbody>
         </table>
-
-    <form class="form-horizontal">
-        <fieldset>
-            <div class="form-actions">
-                <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-            </div>
-        </fieldset>
-    </form>
+		<form class="form-horizontal">
+			<fieldset>
+				<div class="form-actions">
+					<a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+					<a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+				</div>
+            </fieldset>
+        </form>
     </div>
 </div>
-
-<?php include_file('desktop', 'Freebox_OS', 'js' , 'Freebox_OS'); ?>
+<?php include_file('desktop', 'Freebox_OS', 'js', 'Freebox_OS'); ?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
