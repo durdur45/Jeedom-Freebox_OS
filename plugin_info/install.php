9bec9066bd0960f63777186197d4dfa75e6aa78f
Halbout Michael <michaelhalbout@gmail.com>
2017-06-07 08:13:35 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -1,7 +1,19 @@
 <?php
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function Freebox_OS_install() {
+function Freebox_OS_install() {	
+	config::save('FREEBOX_SERVER_IP', config::byKey('FREEBOX_SERVER_IP','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_APP_TOKEN', config::byKey('FREEBOX_SERVER_APP_TOKEN','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_APP_ID', config::byKey('FREEBOX_SERVER_APP_ID','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_TRACK_ID', config::byKey('FREEBOX_SERVER_TRACK_ID','FreeboxOS'),'Freebox_OS');
+	foreach(eqLogic::byType('FreeboxOS') as $eqLogic){
+		foreach($eqLogic->getCmd() as $cmd){			
+			$cmd->setEqType('Freebox_OS');
+			$cmd->save();
+		}
+		$eqLogic->setEqType_name('Freebox_OS');
+		$eqLogic->save();
+	}
 	Freebox_OS::CreateArchi();
 }
 
