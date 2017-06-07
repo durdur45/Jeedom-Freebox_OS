b64f06e1eeddd5aa5aa32fcabadf3ef2649ad7aa
Halbout Michael <michaelhalbout@gmail.com>
2017-06-07 07:59:57 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -6,6 +6,18 @@ function Freebox_OS_install() {
 }
 
 function Freebox_OS_update() {
+	config::save('FREEBOX_SERVER_IP', config::byKey('FREEBOX_SERVER_IP','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_APP_TOKEN', config::byKey('FREEBOX_SERVER_APP_TOKEN','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_APP_ID', config::byKey('FREEBOX_SERVER_APP_ID','FreeboxOS'),'Freebox_OS');
+	config::save('FREEBOX_SERVER_TRACK_ID', config::byKey('FREEBOX_SERVER_TRACK_ID','FreeboxOS'),'Freebox_OS');
+	foreach(eqLogic::byType('FreeboxOS') as $eqLogic){
+		foreach($eqLogic->getCmd() as $cmd){			
+			$cmd->setEqType('Freebox_OS');
+			$cmd->save();
+		}
+		$eqLogic->setEqType_name('Freebox_OS');
+		$eqLogic->save();
+	}
 	Freebox_OS::CreateArchi();    
 }
 
