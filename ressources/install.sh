9551d3b5ef167a732299a3a906e664f4b75abe94
Halbout Michael <michaelhalbout@gmail.com>
2017-11-27 08:56:01 +0100

    Update install.sh

--- ressources/install.sh
+++ ressources/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 1 > /tmp/compilation_Freebox_OS_in_progress
+sudo apt-get install -qy android-tools-adb
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes netcat
 echo 100 > /tmp/compilation_Freebox_OS_in_progress
 rm /tmp/compilation_Freebox_OS_in_progress
