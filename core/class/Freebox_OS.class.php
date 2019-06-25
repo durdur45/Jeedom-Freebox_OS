f3ef51c08405e2d2ba46edc58655f5e50d39b2c1
Halbout Michael <michaelhalbout@gmail.com>
2019-06-25 09:33:00 +0200

    Update Freebox_OS.class.php

--- core/class/Freebox_OS.class.php
+++ core/class/Freebox_OS.class.php
@@ -457,8 +457,7 @@ class Freebox_OS extends eqLogic {
 							$result=$FreeboxAPI->getTile($Equipement->getLogicalId());
 							if($result!=false){
 								foreach($result['data'] as $Commande){
-									if(is_object($Commande))
-										$Equipement->checkAndUpdateCmd($Commande['ep_id'],$Commande['value']);
+									$Equipement->checkAndUpdateCmd($Commande['ep_id'],$Commande['value']);
 								}
 							}
 						break;
